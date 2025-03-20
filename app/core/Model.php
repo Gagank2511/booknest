5ad56e3cbd4ce2afe5801d4e4d511b3c9b2ae067
Kaur <gagandeepmalh@gmail.com>
2025-03-20 21:45:11 +0000

    created a core file for model

---
+++ app/core/Model.php
@@ -0,0 +1,25 @@
+<?php
+
+class Model
+{
+    protected $db;
+
+    public function __construct()
+    {
+        //use port 8889 on a mac
+        $dsn = 'mysql:host=localhost;port=3306;dbname=misc;charset=utf8mb4';
+        $username = 'root';
+        $password = '';
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $this->db = new PDO($dsn, $username, $password, $options);
+        } catch (PDOException $e) {
+            die('Database connection failed: ' . $e->getMessage());
+        }
+    }
+}
