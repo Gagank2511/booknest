bd08d0ba52b1846fb31c7dda94ab3548037862b4
Kaur <gagandeepmalh@gmail.com>
2025-03-28 22:17:32 +0000

    Added search form

--- app/views/books/index.php
+++ app/views/books/index.php
@@ -9,12 +9,20 @@
     <?php include "../app/views/components/header.php"; ?>
 
     <h1>Book Catalogue</h1>
+    <form action="/books" method="GET">
+        <input type="text" name="search" placeholder="Search for books..." required>
+        <button type="submit">Search</button>
+    </form>
     <ul>
-        <?php foreach ($data['books'] as $book): ?>
-            <li>
-                <a href="/books/show/<?= $book['id'] ?>"><?= htmlspecialchars($book['title']) ?></a>
-            </li>
-        <?php endforeach; ?>
+        <?php if(empty($data['books'])): ?>
+            <li>No books found.</li>
+        <?php else: ?>
+            <?php foreach ($data['books'] as $book): ?>
+                <li>
+                    <a href="/books/show/<?= $book['id'] ?>"><?= htmlspecialchars($book['title']) ?></a> by <?= htmlspecialchars($book['author']) ?>
+                </li>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </ul>
     <?php include "../app/views/components/footer.php"; ?>
 </body>
