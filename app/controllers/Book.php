a19fe6be92949d6b3956bcd11719dade2aaf5629
Kaur <gagandeepmalh@gmail.com>
2025-03-28 22:29:07 +0000

    Modified the index fn

--- app/controllers/Book.php
+++ app/controllers/Book.php
@@ -3,8 +3,14 @@
 class Book extends Controller {
     public function index() {
         $bookModel = $this->model( 'BookCollection' );
-        $books = $bookModel->getAllBooks();
-        $this->view( 'books/index', [ 'books' => $books ] );
+        //Check if a search term is provided
+        if (isset($_GET['search'])){
+            $searchTerm = trim($_GET['search']);
+            $books = $bookModel->searchBooks($searchTerm); //search for books
+        } else {
+            $books = $bookModel->getAllBooks(); //get all books
+        }
+        $this->view( 'books/index', [ 'books' => $books ] ); //pass books to the view
     }
 
     public function show( $id ) {
