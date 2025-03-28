21684175ccc1c3637de4d970d0fe8f400792c9f6
Kaur <gagandeepmalh@gmail.com>
2025-03-28 19:25:49 +0000

    Added the isset method to initialise the cart session

--- app/controllers/Cart.php
+++ app/controllers/Cart.php
@@ -1,16 +1,20 @@
 <?php
 
 class Cart extends Controller {
-    public function addToCart($bookId, $quantity){
-        $_SESSION['cart'][$bookId] = $quantity;
+    public function addToCart( $bookId, $quantity ) {
+        // Starting the cart session if it is not already set
+        if ( !isset ( $_SESSION[ 'cart' ] ) ) {
+            $_SESSION[ 'cart' ] = [];
+        }
     }
 
-    public function getCartItems(){
-        return $_SESSION['cart'] ?? [];
+    // 
+    public function getCartItems() {
+        return $_SESSION[ 'cart' ] ?? [];
     }
 
-    public function clearCart(){
-        unset($_SESSION['cart']);
+    public function clearCart() {
+        unset( $_SESSION[ 'cart' ] );
     }
 
 }
