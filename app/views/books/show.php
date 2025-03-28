b2593218d0810edf4ec23be8e9515ca894ebf06b
Kaur <gagandeepmalh@gmail.com>
2025-03-28 10:16:37 +0000

    Changed the title, chnaged book to books in body

--- app/views/books/show.php
+++ app/views/books/show.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= htmlspecialchars($data['book']['title']) ?></title>
+    <title></title>
 </head>
+
 <body>
-    <h1><?= htmlspecialchars($data['book']['title']) ?></h1>
-    <p>Author: <?= htmlspecialchars($data['book']['author']) ?></p>
-    <p>Description: <?= htmlspecialchars($data['book']['description']) ?></p>
-    <p>Price: $<?= htmlspecialchars($data['book']['price']) ?></p>
+    <?php if (sizeof($data['books']) == 0) : ?>
+        There are no books to show
+
+    <?php
+        die();
+    endif; ?>
+    <h1><?= htmlspecialchars($data['books']['title']) ?></h1>
+    <p>Author: <?= htmlspecialchars($data['books']['author']) ?></p>
+    <p>Description: <?= htmlspecialchars($data['books']['description']) ?></p>
+    <p>Price: $<?= htmlspecialchars($data['books']['price']) ?></p>
 
     <h2>Leave a Review</h2>
-    <form action="/reviews/add/<?= $data['book']['id'] ?>" method="POST">
+    <form action="/reviews/add/<?= $data['books']['id'] ?>" method="POST">
         <label>Rating:</label>
         <input type="number" name="rating" min="1" max="5" required>
 
@@ -32,4 +40,5 @@
         <?php endforeach; ?>
     </ul>
 </body>
+
 </html>
