9510b32570567f6e39873a912efec10ed043e3a8
Kaur <gagandeepmalh@gmail.com>
2025-03-28 11:47:37 +0000

    Added admin link

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -3,5 +3,10 @@
         <a href="/">Home</a>
         <a href="/register">Register</a>
         <a href="/login">Login</a>
+        <?php 
+        if(isset($_SESSION['user_id']) && $_SESSION['user_id']==1) {
+            echo '<a href="/admin">Admin</a>';
+        }
+        ?>
     </nav>
 </header>
