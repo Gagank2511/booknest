b4c88e7ad32c72c06eaad4a4b8734d2de39db985
Kaur <gagandeepmalh@gmail.com>
2025-03-28 09:40:01 +0000

    Setup database once

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
-session_start();
-
 require_once '../app/core/Main.php';
 require_once '../app/core/Controller.php';
 require_once '../app/core/Model.php';
+
+// call setup once
+// require_once '../app/models/Setup.php';
+// echo "installing";
+// $setup = new Setup;
+// $setup->initialSetup();
+
 
 // Start the app
 $app = new Main();
