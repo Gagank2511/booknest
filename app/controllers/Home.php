15ef17a9ea276d7f685c226949d299de35d4f533
Kaur <gagandeepmalh@gmail.com>
2025-03-31 18:08:12 +0100

    Sanitised input to prevent XSS attacks

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,8 +2,30 @@
 
 class Home extends Controller
 {
+    /**
+     * Loads the home page view with a sanitized title.
+     *
+     * @return void
+     */
     public function index()
     {
-        $this->view('home/index', ['title' => 'Home Page']);
+        try {
+            $title = $this->sanitizeInput('Home Page');
+            $this->view('home/index', ['title' => $title]);
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            $this->view('error/index', ['message' => 'An error occurred while loading the page.']);
+        }
+    }
+
+    /**
+     * Sanitize input to prevent XSS attacks.
+     *
+     * @param string $input
+     * @return string
+     */
+    private function sanitizeInput($input)
+    {
+        return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
     }
 }
