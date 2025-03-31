8b1957208840aa21240c9f4d224d96a9c604cd59
Kaur <gagandeepmalh@gmail.com>
2025-03-31 14:12:41 +0100

    Minor changes

--- app/controllers/Cart.php
+++ app/controllers/Cart.php
@@ -1,7 +1,7 @@
 <?php
 
 class Cart extends Controller {
-    public function addToCart( $bookId, $quantity ) {
+    public function addToCart(int $bookId, int $quantity ) {
         // Starting the cart session if it is not already set
         if ( !isset ( $_SESSION[ 'cart' ] ) ) {
             $_SESSION[ 'cart' ] = [];
