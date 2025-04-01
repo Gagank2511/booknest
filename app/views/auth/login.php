ad967007e14bdb2b9817d928360baf0c47fccb88
Kaur <gagandeepmalh@gmail.com>
2025-04-01 18:26:34 +0100

    Minor changes

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -14,15 +14,15 @@
     <h2>Login</h2>
 
     <?php if (!empty($data['error'])): ?>
-        <p style="color: red;"><?= htmlspecialchars($data['error']) ?></p>
+        <p style="color: red;"><?= htmlspecialchars($data['error'], ENT_QUOTES, 'UTF-8') ?></p>
     <?php endif; ?>
 
     <form action="/login/authenticate" method="POST">
         <label>Email:</label>
-        <input type="email" name="email" required>
+        <input type="email" name="email" id="email" required>
 
         <label>Password:</label>
-        <input type="password" name="password" required>
+        <input type="password" name="password" id="password" required>
 
         <button type="submit">Login</button>
     </form>
