af70eeba41571a001394782639e74acdbd7d8000
Kaur <gagandeepmalh@gmail.com>
2025-04-01 18:18:06 +0100

    minor changes

--- app/views/admin/addBook.php
+++ app/views/admin/addBook.php
@@ -11,20 +11,20 @@
 
     <h1>Add New Book</h1>
     <?php if(!empty($data['error'])): ?>
-        <p style="color: red;"><?= htmlspecialchars($data['error']) ?></p>
+        <p style="color: red;"><?= htmlspecialchars($data['error'], ENT_QUOTES, 'UTF-8') ?></p>
         <?php endif; ?>
         <form action="/admin/addBook" method="POST">
             <label for="title">Title:</label>
-            <input type="text" name="title" required>
+            <input type="text" name="title" id="title" required>
 
             <label for="author">Author:</label>
-            <input type="text" name="author" required>
+            <input type="text" name="author" id="author">
 
             <label for="description">Description:</label>
-            <textarea name="description"></textarea>
+            <textarea name="description" id="description"></textarea>
 
             <label for="price">Price:</label>
-            <input type="number" name="price" step="0.01" required>
+            <input type="number" name="price" id="price" step="0.01" required>
 
             <button type="submit">Add Book</button>
 
