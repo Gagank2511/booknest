dff0f8360044eaf9407e5a2d0f13cf5fd9837877
Kaur <gagandeepmalh@gmail.com>
2025-04-01 19:46:46 +0100

    Added books to nav bar

--- app/views/components/header.php
+++ app/views/components/header.php
@@ -3,6 +3,9 @@
         <a href="/">Home</a>
         <a href="/register">Register</a>
         <a href="/login">Login</a>
+        <a href="/books/index">Books</a>
+
+
         <?php 
         if(isset($_SESSION['user_id']) && $_SESSION['user_id']==1) {
             echo '<a href="/admin">Admin</a>';
