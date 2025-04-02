81e2dccd4aa8e133ad9b017b207f2df220782215
Kaur <gagandeepmalh@gmail.com>
2025-04-02 12:04:57 +0100

    Removed the int argument

--- app/controllers/Book.php
+++ app/controllers/Book.php
@@ -1,28 +1,35 @@
 <?php
+
 class Book extends Controller {
     public function index() {
-        $bookModel = $this->model('BookCollection');
-        $books = $bookModel->getAllBooks(); // Fetch all books
-        $this->view('book/index', ['books' => $books]); // Pass books to the view
+        $bookModel = $this->model( 'BookCollection' );
+        $books = $bookModel->getAllBooks();
+        // Fetch all books
+        $this->view( 'book/index', [ 'books' => $books ] );
+        // Pass books to the view
     }
-    public function show($id){
-        $bookModel = $this->model('BookCollection');
-        $book = $bookModel->getBookById($id);
-        $reviewModel = $this->model("Review");
-        $bookReview = $reviewModel->getReviewsByBookId($id);
-        $this->view('book/show', ['book' => $book, 'review'=>$bookReview]);
+
+    public function show( $id ) {
+        var_dump($_SESSION);
+        $bookModel = $this->model( 'BookCollection' );
+        $book = $bookModel->getBookById( $id );
+        $reviewModel = $this->model( 'Review' );
+        $bookReview = $reviewModel->getReviewsByBookId( $id );
+        $this->view( 'book/show', [ 'book' => $book, 'review'=>$bookReview ] );
     }
 
     public function search() {
-        $bookModel = $this->model('BookCollection');
+        $bookModel = $this->model( 'BookCollection' );
         $books = [];
 
-        if (isset($_GET['search'])) {
-            $searchTerm = trim($_GET['search']);
-            $books = $bookModel->searchBooks($searchTerm); // Search for books
+        if ( isset( $_GET[ 'search' ] ) ) {
+            $searchTerm = trim( $_GET[ 'search' ] );
+            $books = $bookModel->searchBooks( $searchTerm );
+            // Search for books
         }
 
-        $this->view('book/index', ['books' => $books]); // Pass search results to the view
+        $this->view( 'book/index', [ 'books' => $books ] );
+        // Pass search results to the view
     }
 }
 ?>
